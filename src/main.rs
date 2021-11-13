73490edcd7d5c04f5834b1a7435a2b66dfac7aea
elsuizo <mnoblia@disroot.org>
2021-11-13 19:55:32 -0300

    remove all the unnecesary imports for the blinky

--- src/main.rs
+++ src/main.rs
@@ -15,24 +15,11 @@
 use panic_semihosting as _;
 use rtic::app;
 
-// NOTE(elsuizo:2021-11-13): this dispatchers thing is the old extern "C" in the v0.5 and its the
-// interrupt source that we attatch to the software task
 #[app(device = stm32f1xx_hal::pac, dispatchers = [EXTI2])]
 mod app {
-    // NOTE(elsuizo:2021-11-13): now the includes go here???
-    use stm32f1xx_hal::gpio::{Output, State};
-    use stm32f1xx_hal::{
-        gpio,
-        i2c::{BlockingI2c, DutyCycle, I2c, Mode},
-        pac,
-        prelude::*,
-        rtc::Rtc,
-        serial::{self, Config, Serial},
-        stm32,
-        time::Hertz,
-        timer,
-    };
-    use systick_monotonic::*; // Implements the `Monotonic` trait
+    use stm32f1xx_hal::gpio::State;
+    use stm32f1xx_hal::{gpio, prelude::*};
+    use systick_monotonic::*;
 
     //-------------------------------------------------------------------------
     //                        type alias
@@ -40,8 +27,11 @@ mod app {
     type Led = gpio::gpioc::PC13<gpio::Output<gpio::PushPull>>;
     // A monotonic timer to enable scheduling in RTIC
     #[monotonic(binds = SysTick, default = true)]
-    type MyMono = Systick<30>; // 30 Hz
+    type MyMono = Systick<72>; // 30 Hz
 
+    //-------------------------------------------------------------------------
+    //                        resources declaration
+    //-------------------------------------------------------------------------
     // Resources shared between tasks
     #[shared]
     struct Shared {}
@@ -51,40 +41,28 @@ mod app {
         led: Led,
     }
 
+    //-------------------------------------------------------------------------
+    //                        initialization fn
+    //-------------------------------------------------------------------------
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         //-------------------------------------------------------------------------
         //                        hardware initialization
         //-------------------------------------------------------------------------
-        // cx.core.DCB.enable_trace();
-        let mut flash = cx.device.FLASH.constrain();
         let mut rcc = cx.device.RCC.constrain();
-        let mut afio = cx.device.AFIO.constrain(&mut rcc.apb2);
-        let mut pwr = cx.device.PWR;
-        let mut backup_domain = rcc.bkp.constrain(cx.device.BKP, &mut rcc.apb1, &mut pwr);
-        let clocks = rcc
-            .cfgr
-            .use_hse(8.mhz())
-            .sysclk(72.mhz())
-            .pclk1(36.mhz())
-            .freeze(&mut flash.acr);
-        let mut gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
-        let mut gpiob = cx.device.GPIOB.split(&mut rcc.apb2);
         let mut gpioc = cx.device.GPIOC.split(&mut rcc.apb2);
-        let mut led = gpioc
+        let led = gpioc
             .pc13
             .into_push_pull_output_with_state(&mut gpioc.crh, State::Low);
         //-------------------------------------------------------------------------
         //                        rtic initialization
         //-------------------------------------------------------------------------
         let systick = cx.core.SYST;
-        let mono = Systick::new(systick, 16_000_000);
+        let mono = Systick::new(systick, 8_000_000);
 
-        // Spawn the task `bar` 1 second after `init` finishes, this is enabled
+        // Spawn the task `blinky` 1 second after `init` finishes, this is enabled
         // by the `#[monotonic(..)]` above
         blinky::spawn_after(1.secs()).unwrap();
-
-        // debug::exit(debug::EXIT_SUCCESS); // Exit QEMU simulator
 
         (Shared {}, Local { led }, init::Monotonics(mono))
     }
