c2527a6b08ad0461970d2984d173a3c61ef01301
elsuizo <mnoblia@disroot.org>
2021-12-06 20:12:51 -0300

    update

--- src/ui.rs
+++ src/ui.rs
@@ -19,7 +19,7 @@ pub fn draw_menu<D>(target: &mut D, state: MenuState) -> Result<(), D::Error>
 where
     D: DrawTarget<Color = BinaryColor>,
 {
-    let logo_image = ImageRawLE::new(include_bytes!("../Images/very_logo.raw"), 64);
+    let logo_image = ImageRawLE::new(include_bytes!("../Images/rust.raw"), 64);
     // normal text
     let normal = MonoTextStyleBuilder::new()
         .font(&FONT_9X15)
